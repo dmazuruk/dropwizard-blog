512423041dbcb806ab377b200c5c7ce58098e5ca
dmazuruk <dmazuruk@gmail.com>
2014-07-20 21:51:52 +0200

    Added first resource

---
+++ src/main/java/pl/dmazuruk/resource/IndexResource.java
@@ -0,0 +1,26 @@
+package pl.dmazuruk.resource;
+
+import com.codahale.metrics.annotation.Timed;
+import pl.dmazuruk.model.Blog;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by dmazuruk on 7/20/14.
+ */
+@Path("/")
+public class IndexResource {
+
+    @GET
+    @Produces(value = MediaType.APPLICATION_JSON)
+    @Timed
+    public List<Blog> index() {
+        return Arrays.asList(new Blog("Fake dmazuruk's blog",
+                "https://dmazuruk.com/fake-blog"));
+    }
+}
